05496a6e7c7cf484dcd6a21d57739f693e641184
jingwen <jingwen.wang@scilifelab.se>
2021-01-28 09:35:08 +0000

    add UMI group tag to bam file

--- correct_pair.py
+++ correct_pair.py
@@ -1,6 +1,8 @@
 import sys
 import os
 import pysam
+import csv
+from argparse import ArgumentParser
 
 class ReadPair(object):
     def __init__(self,read):
@@ -11,8 +13,11 @@ class ReadPair(object):
         self.r1f2 = 0
         self.f1f2 = 0
         self.r1r2 = 0
+        self.BX = None
         self.update_mate(read)
 
+    def update_UMI_group(self, umi_dict):
+        self.BX = umi_dict[self.id]
 
     def update_mate(self,read):
         if read.is_read1:
@@ -26,6 +31,8 @@ class ReadPair(object):
         self.read2.is_proper_pair = True
         self.id = f"{self.id}#{order}"
         self.read1.query_name = self.read2.query_name = self.id
+        self.read1.set_tag("BX", self.BX)
+        self.read2.set_tag("BX", self.BX)
 
     def check_proper_pair(self):
         write_output = False
@@ -33,7 +40,7 @@ class ReadPair(object):
             if not self.read2.is_reverse: ##If read 1 is reverse and read 2 is forward, count the pair as F2R1, tag it, and mark for inclusion in the output bam.
                 self.r1f2 += 1
                 write_output = True
-                self.update_proper_pair("RF") 
+                self.update_proper_pair("RF")
             else:
                 self.r1r2 += 1 ##if read 1 is reverse and read 2 is also reverse, count it as R1R2 and DO NOT mark for inclusion in final bam.
         else:
@@ -46,39 +53,69 @@ class ReadPair(object):
         ##write out the pairs which are either F1R2 or F2R1 to the output; write out the counters for classifying reads
         return write_output, self.f1r2, self.r1f2, self.f1f2, self.r1r2
 
+def main():
+    parser = ArgumentParser()
+    parser.add_argument(
+        '--input',
+        dest = 'in_bam',
+        required = True,
+        help = 'Path to aligned, paired-end, bam file.'
+        )
+    parser.add_argument(
+        "--UMI-group",
+        dest='umi_group',
+        help="Path to grouped UMI file (tsv format)."
+    )
 
-input_file = pysam.AlignmentFile(sys.argv[1],"rb")
-tmp_file = pysam.AlignmentFile("tmp.bam","wb",template = input_file)
-read_pairs = dict()
-pair_types = [0,0,0,0]
+    arg = parser.parse_args()
+    input_file = pysam.AlignmentFile(arg.in_bam,"rb")
+    tmp_file = pysam.AlignmentFile("tmp.bam","wb",template = input_file)
+    read_pairs = dict()
+    umi_group_dict = dict()
+    pair_types = [0,0,0,0]
 
-##When a read name is first encountered and gets added to read_pairs dict, does the loop then go over it again? Why? If not, how do both read 1 and read 2 get added to the ReadPairs object?
-for read in input_file.fetch(until_eof=True):
-    if not read.is_unmapped and not read.mate_is_unmapped and read.next_reference_id == read.reference_id: ##if both read and mate are mapped and their IDs match
-        name = read.query_name
-        if name in read_pairs.keys(): ##if read name already in dict
-            rp = read_pairs[name] ##rp is now the ReadPairs class object
-            rp.update_mate(read) ##writes the read to either read1 or read2 attribute of the ReadPairs object, depending on whether it is read 1 or 2.
-            write_out, F1R2, R1F2, F1F2, R1R2 = rp.check_proper_pair() ##check_proper_pair will classify reads as F1R2, F2R1, F1F2, or R1R2 and mark for inclusion (write_out = TRUE) only the former two categories. It will also have added an FR or RF tag to the end of the read name. 
-            pair_types = [x + y for x, y in zip(pair_types, [F1R2, R1F2, F1F2, R1R2])] ##pair_types is a counter for the type of pair, for each read, one of these four categories will be a 1 after check_proper_pair and the others will be 0, so this 1 is hereby added to the proper position in counter.
-            if write_out: ##write out the reads marked for it to bam.
-                tmp_file.write(rp.read1)
-                tmp_file.write(rp.read2)
-            else:
-                pass
-        else: ##if read is not in dict yet, add it to read_pairs dict by converting it into a ReadPair class object. 
-            read_pairs[name] = ReadPair(read)
-input_file.close()
-tmp_file.close()
+    if arg.umi_group:
+        group_file = open(arg.umi_group)
+        group_info = csv.reader(group_file, delimiter="\t")
+        umi_group_dict = {x[0]:x[6] for x in group_info}
+        group_file.close()
 
-output_bam = sys.argv[1].replace(".bam","_properPair.bam")
-pysam.sort("-o",output_bam,"tmp.bam")
-pysam.index(output_bam)
-os.remove("tmp.bam")
+    ##When a read name is first encountered and gets added to read_pairs dict, does the loop then go over it again? Why? If not, how do both read 1 and read 2 get added to the ReadPairs object?
+    for read in input_file.fetch(until_eof=True):
+        ##if both read and mate are mapped and their IDs match
+        if not read.is_unmapped and not read.mate_is_unmapped and read.next_reference_id == read.reference_id:
+            name = read.query_name
+            if name in read_pairs.keys(): ##if read name already in dict
+                rp = read_pairs[name] ##rp is now the ReadPairs class object
+                ##writes the read to either read1 or read2 attribute of the ReadPairs object, depending on whether it is read 1 or 2.
+                rp.update_mate(read)
+                ##check_proper_pair will classify reads as F1R2, F2R1, F1F2, or R1R2 and mark for inclusion (write_out = TRUE) only the former two categories. It will also have added an FR or RF tag to the end of the read name.
+                write_out, F1R2, R1F2, F1F2, R1R2 = rp.check_proper_pair()
+                pair_types = [x + y for x, y in zip(pair_types, [F1R2, R1F2, F1F2, R1R2])] ##pair_types is a counter for the type of pair, for each read, one of these four categories will be a 1 after check_proper_pair and the others will be 0, so this 1 is hereby added to the proper position in counter.
+                if write_out: ##write out the reads marked for it to bam.
+                    tmp_file.write(rp.read1)
+                    tmp_file.write(rp.read2)
+                else:
+                    pass
+            else: ##if read is not in dict yet, add it to read_pairs dict by converting it into a ReadPair class object.
+                newRP = ReadPair(read)
+                if arg.umi_group:
+                    newRP.update_UMI_group(umi_group_dict)
+                read_pairs[name] = newRP
+    input_file.close()
+    tmp_file.close()
 
-stat_file = open(sys.argv[1].replace(".bam","_readPair.stat"),"w")
-stat_file.write("#Read pairs in F1R2: " + str(pair_types[0]) + "\n")
-stat_file.write("#Read pairs in R1F2: " + str(pair_types[1]) + "\n")
-stat_file.write("#Read pairs in F1F2: " + str(pair_types[2]) + "\n")
-stat_file.write("#Read pairs in R1R2: " + str(pair_types[3]) + "\n")
-stat_file.close()
+    output_bam = arg.in_bam.replace(".bam","_properPair.bam")
+    pysam.sort("-o",output_bam,"tmp.bam")
+    pysam.index(output_bam)
+    os.remove("tmp.bam")
+
+    stat_file = open(arg.in_bam.replace(".bam","_readPair.stat"),"w")
+    stat_file.write("#Read pairs in F1R2: " + str(pair_types[0]) + "\n")
+    stat_file.write("#Read pairs in R1F2: " + str(pair_types[1]) + "\n")
+    stat_file.write("#Read pairs in F1F2: " + str(pair_types[2]) + "\n")
+    stat_file.write("#Read pairs in R1R2: " + str(pair_types[3]) + "\n")
+    stat_file.close()
+
+if __name__ == "__main__":
+    main()
